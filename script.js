5fd9840e25ca3d74b9c1c5d41ee6c43a9d10fc5e
Nob Yuasys <yuasys@gmail.com>
2025-02-09 07:07:12 +0900

    スタイル調整とメニュー閉を追加

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function toggleMenu() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+// メニューを閉じる関数
+function closeMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.remove('active');
+}
